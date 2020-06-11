ee2c7eed3a73db649a957990106b7d002ebd6521
Deepti <deepti.sharma@gmail.com>
2020-06-11 20:34:55 +0200

    java basics

---
+++ javascript/javascript1/week1/test1.js
@@ -0,0 +1,31 @@
+//JS Week1 (5-10-2020)
+
+/*/display
+console.log('hello world');
+console.log(12);
+
+//var variables
+var num = 10;
+console.log('the number is: ' + num);
+
+//let variables
+var num1 = 10;
+console.log('the number1 is: ' + num1);
+var num1 = 'florence';
+console.log('the number1 is: ' + num1);
+
+//ES6 binds the variable datatype
+let num2 = 'florence again';
+console.log('the number2 is: ' + num2);
+let num2 = 100; //error
+console.log('the number2 is: ' + num2);
+
+//constants
+const num3 = 3.14;
+console.log('the number3 is: ' + num3);
+num3 = 668767; //error*/
+
+//arrays
+const arr = ['abc', 30, 'india'];
+console.log(arr.indexOf(30));
+console.log(arr.findIndex('india'));
